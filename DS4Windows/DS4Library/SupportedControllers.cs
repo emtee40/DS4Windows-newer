f77ec86620c02bd11e23d441b40413134ebf212d
jdfeng <jdfeng@gmail.com>
2018-04-23 00:08:24 -0700

    Clean up unused code

--- DS4Windows/DS4Library/SupportedControllers.cs
+++ DS4Windows/DS4Library/SupportedControllers.cs
@@ -24,30 +24,21 @@ namespace DS4Windows {
 
     public class VidPidInfo : ConfigurationElement {
 
-        public VidPidInfo() { }
-
-        private readonly int vid, pid;
-
-        public VidPidInfo(int vid, int pid) {
-            this.vid = vid;
-            this.pid = pid;
-        }
-
         [ConfigurationProperty("name", IsRequired = true)]
         public string Name { get { return (string) this["name"]; } }
 
         [ConfigurationProperty("vid", IsRequired = true)]
-        private string _vid { get { return vid != 0 ? vid.ToString("X") : (string) this["vid"]; } }
+        private string _vid { get { return (string) this["vid"]; } }
 
         [ConfigurationProperty("pid", IsRequired = true)]
-        private string _pid { get { return pid != 0 ? pid.ToString("X") : (string) this["pid"]; } }
+        private string _pid { get { return (string) this["pid"]; } }
 
         // Explictly convert the string values to a consistent value for hashing
         public string ID { get { return VID.ToString("X") + "_" + PID.ToString("X");  } }
 
-        public int VID { get { return vid != 0 ? vid : int.Parse(_vid, NumberStyles.AllowHexSpecifier); } }
+        public int VID { get { return int.Parse(_vid, NumberStyles.AllowHexSpecifier); } }
 
-        public int PID { get { return pid != 0 ? pid : int.Parse(_pid, NumberStyles.AllowHexSpecifier); } }
+        public int PID { get { return int.Parse(_pid, NumberStyles.AllowHexSpecifier); } }
 
         /** Returns true if the vidpid details match those of the specified HidDevice **/
         public bool matchesHid(HidDevice hidDevice) {
